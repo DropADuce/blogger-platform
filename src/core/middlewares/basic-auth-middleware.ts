24443a9767e67e2637d8b0cddbaa5ddd3fe289d9
Druce <lapuzin@tochka.com>
2026-02-04 00:58:06 +0500

    fix: Незашифрованная строка вызывала 500

--- src/core/middlewares/basic-auth-middleware.ts
+++ src/core/middlewares/basic-auth-middleware.ts
@@ -6,9 +6,15 @@ const PASSWORD = 'qwerty';
 
 export const basicAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const [, credentials64] = (req.headers.authorization ?? '').split(' ');
-  const [login, password] = atob(credentials64 ?? '').split(':');
 
-  if (login !== LOGIN || password !== PASSWORD) return res.sendStatus(HTTP_STATUS.UNAUTHORIZED);
+  try {
+    const [login, password] = atob(credentials64 ?? '').split(':');
+
+    if (login !== LOGIN || password !== PASSWORD) throw new Error();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_: unknown) {
+    return res.sendStatus(HTTP_STATUS.UNAUTHORIZED);
+  }
 
   return next();
 }
