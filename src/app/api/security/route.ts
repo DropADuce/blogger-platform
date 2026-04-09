6c3f294e096c02afa40b70bfb43cecb3ae650131
Druce <lapuzin@tochka.com>
2026-04-10 01:25:52 +0500

    fix: Веротяно на платформе засылается только refreshToken. Проверим

--- src/app/api/security/route.ts
+++ src/app/api/security/route.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express';
-import { withJwtTokenMiddleware } from '../../../core/middlewares/with-jwt-token.middleware';
 import { securityRoute } from './route.handler';
 import { withRefreshTokenMiddleware } from '../../../core/middlewares/with-refresh-token.middleware';
 
 export const router = Router()
-  .get('/devices', withJwtTokenMiddleware, withRefreshTokenMiddleware, securityRoute.getActiveSessions)
+  .get('/devices', withRefreshTokenMiddleware, securityRoute.getActiveSessions)
   .delete(
     '/devices',
-    withJwtTokenMiddleware,
     withRefreshTokenMiddleware,
     securityRoute.removeOtherSessions
   )
   .delete(
     '/devices/:id',
-    withJwtTokenMiddleware,
     withRefreshTokenMiddleware,
     securityRoute.removeCurrentSession
   );
